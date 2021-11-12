b02d5be797cf00d4dc8700bd2b5f3cb3ba2779de
gprogrammin <87392072+NothingButProgrammer@users.noreply.github.com>
2021-11-12 11:59:54 +0200

    version 0.0.3
    Not finished version of the project.

---
+++ LINUXCHEATSHEET.sh
@@ -0,0 +1,46 @@
+echo "
+  ________  _______    ____ ___   /\.__   .__                                   
+ /  _____/  \      \  |    |   \ / /|  |  |__|  ____   __ __ ___  ___           
+/   \  ___  /   |   \ |    |   // / |  |  |  | /    \ |  |  \\  \/  /           
+\    \_\  \/    |    \|    |  // /  |  |__|  ||   |  \|  |  / >    <            
+ \______  /\____|__  /|______// /   |____/|__||___|  /|____/ /__/\_ \           
+        \/         \/         \/                   \/              \/           
+                                                                                
+        .__                      __               .__                     __    
+  ____  |  |__    ____  _____  _/  |_       ______|  |__    ____   ____ _/  |_  
+_/ ___\ |  |  \ _/ __ \ \__  \ \   __\     /  ___/|  |  \ _/ __ \_/ __ \\   __\ 
+\  \___ |   Y  \\  ___/  / __ \_|  |       \___ \ |   Y  \\  ___/\  ___/ |  |   
+ \___  >|___|  / \___  >(____  /|__|      /____  >|___|  / \___  >\___  >|__|   
+     \/      \/      \/      \/                \/      \/      \/     \/        
+                                                                                "
+
+
+
+
+echo "Welcome to GNU/linux cheat sheet!"
+echo "Written on bash"
+echo "for use on GNU/linux systems (Ironic lol)"
+echo "written by gprogrammin (if ya want fork me on github)"
+echo "[PRESS ANY KEY TO START THE TUTORIAL]"
+read start
+echo "Why would you want to use Linux?
+1)Linux is free
+2)Linux is open source which means that the everyone can view the code. This makes the system very safe.
+3)Linux is customizable. You can make it look and feel different, you can change EVERYTHING. That explains why there are so many verisions of linux -
+they are called dstributions."
+
+echo "There are 3 main distros (distributions) on which everything is based on: debian, arch, redhat.
+There are different packages for all of those systems. Packages are programs for your system.
+linux mint - easy to use!
+ubuntu - the most popular one, but a bit harder than linux mint, you may need the knowlage of terminal
+debian - not easy to use, but very interesting!
+pop_os - linux for gamers, easy to use!
+manjaro linux - arch based, not very easy to use, must know terminal.
+elementary os - very nice looking, easy to use"
+
+echo "
+also you need to know that systems can have different desktop enviroments - it's just like windows themes.
+there are 5 main: xfce - for not very powerful computers, kde - a very great looking one, GNOME - what ubuntu uses, cinnamon - something like xfce,
+mate - something like cinnamon and GNOME at the same time. There are also window managers."
+#note: I will add the terminal cheat sheet later, this is the 0.0.3 version of the program
+#maybe a bit more ASCII arts
